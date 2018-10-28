a9497f2430e0090345ff8cd5b88f7de0e47eb9c5
Robby Kraft <robbykraft@gmail.com>
2018-10-28 04:20:09 -0400

    view and interactive view

---
+++ src/interactive.js
@@ -0,0 +1,72 @@
+
+import SVG from "./svg";
+import View from "./view"
+
+export default function Interactive(){
+
+	let {setPadding,
+		 zoomView,
+		 translate,
+		 setViewBox} = View(...arguments);
+
+	// implement these, they will get called when the event fires
+	this.event = {
+		animate: function(){},
+		onResize: function(){},
+		onMouseMove: function(){},
+		onMouseDown: function(){},
+		onMouseUp: function(){},
+		onMouseDidBeginDrag: function(){},
+	}
+
+	// interaction
+	let mouse = {
+		isPressed: false,// is the mouse button pressed (y/n)
+		position: [0,0], // the current position of the mouse
+		pressed: [0,0],  // the last location the mouse was pressed
+		drag: [0,0]      // vector, displacement from start to now
+	};
+
+	var that = this;
+	svg.onmousedown = function(event){
+		mouse.isPressed = true;
+		mouse.pressed = SVG.convertToViewbox(svg, event.clientX, event.clientY);
+		that.event.onMouseDown(mouse);
+	};
+	svg.onmouseup = function(event){
+		mouse.isPressed = false;
+		that.event.onMouseUp(mouse);
+	};
+	svg.onmousemove = function(event){
+		mouse.position = SVG.convertToViewbox(svg, event.clientX, event.clientY);
+		if(mouse.isPressed){
+			mouse.drag = [mouse.position[0] - mouse.pressed[0], 
+			              mouse.position[1] - mouse.pressed[1]];
+			that.event.onMouseDidBeginDrag(mouse);
+		}
+		that.event.onMouseMove(mouse);
+	};
+	svg.onResize = function(event){
+		that.event.onResize(event);
+	};
+
+	// javascript get Date()
+	// todo: watch for the variable getting set
+	animateTimer = setInterval(function(){
+		if(typeof that.event.animate === "function"){
+			that.event.animate({"time":svg.getCurrentTime(), "frame":frameNum});
+		}
+		frameNum += 1;
+	}, 1000/60);
+
+	// return Object.freeze({
+	return {
+		setPadding,
+		zoomView,
+		translate,
+		setViewBox
+		event:this.event,
+		mouse,
+	};
+
+}
---
+++ src/view.js
@@ -0,0 +1,66 @@
+/** svg file viewer
+ * converts .fold file into SVG, binds it to the DOM
+ * @param: (constructor) a DOM object or "string" DOM id
+ *  and this will bind the SVG to it.
+ */
+
+"use strict";
+
+import SVG from "./svg";
+
+export default function View(){
+
+	// create a new SVG
+	let _svg = SVG.svg();
+
+	// view properties
+	let _zoom = 1.0;
+	let _padding = 0.01;
+
+	set padding(pad){
+		_padding = isNaN(pad) ? _padding : pad;
+	}
+
+	set zoom(new_zoom){
+		_zoom = isNaN(new_zoom) ? _zoom : new_zoom;
+	}
+
+	const zoomView = function(scale, origin_x, origin_y){
+		// zoom view
+	}
+
+	const translate = function(dx, dy){
+		// translate view
+	}
+
+	const setViewBox = function(x, y, width, height){
+		SVG.setViewBox(_svg, x, y, width, height, _padding);
+	}
+
+
+	// find a parent element for the new SVG in the arguments
+	document.addEventListener("DOMContentLoaded", function(){
+		//  wait until after the <body> has rendered
+		let args = Array.from(arguments);
+		let element = args.filter((arg) =>
+				arg instanceof HTMLElement)
+			.shift();
+		let idElement = args.filter((a) =>
+				typeof a === "string" || a instanceof String)
+			.map(str => document.getElementById(str))
+			.shift();
+		let parent = (element != null 
+			? element 
+			: (idElement != null 
+				? idElement 
+				: document.body));
+		parent.appendChild(svg);
+	});
+
+	return Object.freeze({
+		setPadding,
+		zoomView,
+		translate,
+		setViewBox
+	});
+}
