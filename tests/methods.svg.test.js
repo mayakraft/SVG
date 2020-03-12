ec66f36c3f214b25f77de1e1265c788657020319
Robby Kraft <robbykraft@gmail.com>
2020-03-12 03:32:03 -0400

    adding beautifier back

--- tests/methods.svg.test.js
+++ tests/methods.svg.test.js
@@ -26,10 +26,10 @@ test("svg export", () => {
   svg.line(0, 0, 300, 150).stroke("black").strokeWidth(5);
   const asString = svg.save();
   const asSvg = svg.save({output: "svg"});
-//   const expectedString = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-// 	<line x1="0" y1="0" x2="300" y2="150" stroke="black" stroke-width="5"/>
-// </svg>`;
-  const expectedString = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg"><line x1="0" y1="0" x2="300" y2="150" stroke="black" stroke-width="5"/></svg>`;
+  const expectedString = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+	<line x1="0" y1="0" x2="300" y2="150" stroke="black" stroke-width="5"/>
+</svg>`;
+  // const expectedString = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg"><line x1="0" y1="0" x2="300" y2="150" stroke="black" stroke-width="5"/></svg>`;
   expect(asString).toBe(expectedString);
   expect(asSvg.childNodes.length).toBe(1);
   expect(asSvg.childNodes[0].nodeName).toBe("line");
