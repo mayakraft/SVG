2657ddc5cf67eb9a82d862b72cbf31e9d3eda3cc
Robby Kraft <robbykraft@gmail.com>
2020-03-24 02:21:54 -0400

    replace innerHTML with createTextNode for nodejs compatibility

--- examples/code/examples/bezier.js
+++ examples/code/examples/bezier.js
@@ -1,10 +1,18 @@
 svg.size(800, 800);
 
+// create a layer behind everything
 var back = svg.g();
+
+// shapes on top
+var curve = svg.path();
 var l1 = svg.line().stroke("black");
 var l2 = svg.line().stroke("black");
-var curve = svg.path();
 
+// these are persistent control points
+// 1.create an svg element to track with each point
+// 2.assign a position
+// 3.append to a parent
+// 4.onChange event handler with "this" bound to the controls
 svg.controls(4)
   .svg(function () { return SVG.circle(0, 0, svg.getWidth() * 0.05).fill("#e53"); })
   .position(function () { return [random(svg.getWidth()), random(svg.getHeight())]; })
